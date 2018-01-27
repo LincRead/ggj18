6159621ee46886793465ea6ce7406aca1cfb4e3d
LincRead <andre.l.read@gmail.com>
2018-01-27 10:20:08 +1000

    Retry push folder structure

---
+++ Assets/Scripts/CameraPixel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraPixel : MonoBehaviour
+{
+    [Range(1f, 4f)]
+    public float pixelScale = 1;
+
+    private int pixelsPerUnit = 100;
+    private float halfScreen = 0.5f;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+        _camera.orthographic = true;
+    }
+
+    void Update()
+    {
+        _camera.orthographicSize = Screen.height * ((halfScreen / pixelsPerUnit) / pixelScale);
+    }
+}
