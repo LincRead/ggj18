d25d265ecabe43bf29f2d61b29a138832f70091e
Brigitte Mackenzie <brigitte.mackenzie@gmail.com>
2018-01-27 17:27:38 +1000

    ship controller

--- Assets/Scripts/ShipCotroller.cs
+++ Assets/Scripts/ShipCotroller.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipCotroller : MonoBehaviour {
-
-    private Transform shipTran;
-
     Vector2 veocity = Vector2.zero;
 
     // Use this for initialization
     private void Start () {
-        shipTran = GetComponent<Transform>();
-        veocity.x = -1f;
+        veocity.x = +1f;
     }
 
     private void Update() {
-        Debug.Log(veocity.y);
+      //  Debug.Log(veocity.y);
         
         if (Input.GetKeyDown("down"))
         {
@@ -23,7 +19,6 @@ public class ShipCotroller : MonoBehaviour {
                 veocity.y -= 1f;
             }
         }
-
         if (Input.GetKeyDown("up"))
         {
             if (veocity.y < 3) {
@@ -34,23 +29,26 @@ public class ShipCotroller : MonoBehaviour {
     }
 
     #region
-
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("collision");
         if (collision.gameObject.tag == "planet")
         {
             Debug.Log("explode");
         }
+
         if (collision.gameObject.tag == "signal")
         {
             Debug.Log("call method action");
+            receiveSignal(collision.gameObject.GetComponent<Signal>().signalCommand);
+            Destroy(collision.gameObject);
         }
-
     }
 
     public void receiveSignal(SignalCommand signalCommand) {
-        switch (signalCommand) {
+        Debug.Log("hit");
 
+            switch (signalCommand) {
             case SignalCommand.LEFT:
                 Debug.Log("left");
                 break;
@@ -60,8 +58,7 @@ public class ShipCotroller : MonoBehaviour {
             case SignalCommand.SHIELD:
                 Debug.Log("shield");
                 break;
-        }
+        } 
     }
     #endregion
-
 }
---
+++ Assets/dummyScript_signal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class dummyScript_signal : MonoBehaviour
+{
+    Vector2 veocity = Vector2.zero;
+
+    // Use this for initialization
+    private void Start()
+    {
+        veocity.x = +3f;
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(veocity.x * Time.deltaTime, veocity.y * Time.deltaTime, 0.0f);
+    }
+}
