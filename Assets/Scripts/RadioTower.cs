fd28d344ebd690bc309827b1f5b21fd9dee460e0
Tim <timothy.hart@my.jcu.edu.au>
2018-01-27 16:17:02 +1000

    Added cooldown to signal

--- Assets/Scripts/RadioTower.cs
+++ Assets/Scripts/RadioTower.cs
@@ -8,6 +8,8 @@ public class RadioTower : MonoBehaviour {
     public GameObject RightSignal;
     public GameObject ShieldSignal;
 
+    public bool canSendSignal;
+
     Animator _animator;
 
 	// Use this for initialization
@@ -17,32 +19,43 @@ public class RadioTower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
-            LaunchSignal(SignalCommand.LEFT);
-        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
-            LaunchSignal(SignalCommand.RIGHT);
-        if (Input.GetKeyUp(KeyCode.Space))
-            LaunchSignal(SignalCommand.SHIELD);
+            if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+                LaunchSignal(SignalCommand.LEFT);
+            if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+                LaunchSignal(SignalCommand.RIGHT);
+            if (Input.GetKeyUp(KeyCode.Space))
+                LaunchSignal(SignalCommand.SHIELD);
 	}
 
     public void LaunchSignal(SignalCommand sigCommand)
     {
-        switch (sigCommand)
+        if (canSendSignal)
         {
-            case SignalCommand.LEFT:
-                if(LeftSignal)
-                    Instantiate(LeftSignal, this.transform);
-                break;
-            case SignalCommand.RIGHT:
-                if (RightSignal)
-                    Instantiate(RightSignal, this.transform);
-                break;
-            case SignalCommand.SHIELD:
-                if(ShieldSignal)
-                    Instantiate(ShieldSignal, this.transform);
-                break;
-        }
+            switch (sigCommand)
+            {
+                case SignalCommand.LEFT:
+                    if (LeftSignal)
+                        Instantiate(LeftSignal, this.transform);
+                    break;
+                case SignalCommand.RIGHT:
+                    if (RightSignal)
+                        Instantiate(RightSignal, this.transform);
+                    break;
+                case SignalCommand.SHIELD:
+                    if (ShieldSignal)
+                        Instantiate(ShieldSignal, this.transform);
+                    break;
+            }
 
-        _animator.Play("send");
+            _animator.Play("send");
+            float time = _animator.GetCurrentAnimatorStateInfo(0).length;
+            canSendSignal = false;
+            Invoke("RemoveCoolDown", time);
+        }
+    }
+
+    void RemoveCoolDown()
+    {
+        canSendSignal = true;
     }
 }
