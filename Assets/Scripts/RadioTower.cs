3eb1627d87715c11e9c46c92730c530f33da7859
Tim <timothy.hart@my.jcu.edu.au>
2018-01-27 14:26:28 +1000

    Implemented RadioTower class-- launches the signals on key press

--- Assets/Scripts/RadioTower.cs
+++ Assets/Scripts/RadioTower.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RadioTower : MonoBehaviour {
 
+    public GameObject LeftSignal;
+    public GameObject RightSignal;
+    public GameObject ShieldSignal;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,27 @@ public class RadioTower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+            LaunchSignal(SignalCommand.LEFT);
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+            LaunchSignal(SignalCommand.RIGHT);
+        if (Input.GetKeyUp(KeyCode.Space))
+            LaunchSignal(SignalCommand.SHIELD);
 	}
+
+    public void LaunchSignal(SignalCommand sigCommand)
+    {
+        switch (sigCommand)
+        {
+            case SignalCommand.LEFT:
+                Instantiate(LeftSignal, this.transform);
+                break;
+            case SignalCommand.RIGHT:
+                Instantiate(RightSignal, this.transform);
+                break;
+            case SignalCommand.SHIELD:
+                Instantiate(ShieldSignal, this.transform);
+                break;
+        }
+    }
 }
