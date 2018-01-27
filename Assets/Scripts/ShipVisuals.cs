af575566d8027b6c6904cca4a0e49c28dcb104e3
LincRead <andre.l.read@gmail.com>
2018-01-27 13:58:14 +1000

    Ship flames

---
+++ Assets/Scripts/ShipVisuals.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipVisuals : MonoBehaviour {
+
+    private Animator _animator;
+
+	void Start ()
+    {
+        _animator = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        HandleDebugInput();
+	}
+
+    void HandleDebugInput()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ActivateLeftEngine();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ActivateRightEngine();
+        }
+    }
+
+    public void ActivateLeftEngine()
+    {
+        _animator.Play("left");
+    }
+
+    public void ActivateRightEngine()
+    {
+        _animator.Play("right");
+    }
+}
