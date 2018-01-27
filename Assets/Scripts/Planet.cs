27bd11203b41d72424fdf43b485f275178b9715c
Tim <timothy.hart@my.jcu.edu.au>
2018-01-27 10:48:53 +1000

    Implemented planet class

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet {
+
+    Vector2 location;
+    float mass;
+    float radius;
+    float gravWellRadius;
+
+    public Planet(Vector2 location, float mass, float radius, float gravWellRadius) {
+        this.location = location;
+        this.mass = mass;
+        this.radius = radius;
+        this.gravWellRadius = gravWellRadius;
+    }
+
+    public void Update() {
+        if (IsSpaceshipInGravWell(GameObject.FindGameObjectWithTag("Player").transform.position))
+            PullShip();
+    }
+
+    public bool IsSpaceshipInGravWell(Vector2 spaceshipPos) {
+        if (Vector2.Distance(spaceshipPos, location) < gravWellRadius)
+            return true;
+        return false;
+    }
+
+    void PullShip()
+    {
+        Vector2 gravVelocity = new Vector2(location.x + (mass * radius), location.y + (mass * radius));
+        WorldManager.PullShip(gravVelocity);
+    }
+}
