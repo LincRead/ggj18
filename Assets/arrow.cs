97211f798b42c7d13fff0803948929bed1dd3de8
Brigitte Mackenzie <brigitte.mackenzie@gmail.com>
2018-01-28 13:22:09 +1000

    sound

---
+++ Assets/arrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour {
+    public bool directionBoolean;
+    public int counter;
+
+    // Use this for initialization
+    void Start () {
+        directionBoolean = true;
+        counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Bounce();
+    }
+
+    public void Bounce()
+    {
+        if (directionBoolean) {
+            transform.Translate(Vector3.left * Time.deltaTime);
+            counter += 1;
+            if (counter > 10) {
+                directionBoolean = false;
+            }
+
+        } else if (directionBoolean == false) {
+            transform.Translate(Vector3.right * Time.deltaTime);
+            counter -= 1;
+            if (counter < 0) {
+                directionBoolean = true;
+            }
+        }
+    }
+}
