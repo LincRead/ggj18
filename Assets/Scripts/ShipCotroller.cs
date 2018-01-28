72c23802186721142f1f944c6cf2417389edcd6e
LincRead <andre.l.read@gmail.com>
2018-01-28 15:46:52 +1000

    FINISHED

--- Assets/Scripts/ShipCotroller.cs
+++ Assets/Scripts/ShipCotroller.cs
@@ -55,6 +55,9 @@ public class ShipCotroller : MonoBehaviour {
 
     private void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Escape))
+            Application.Quit();
+
         if(veocity.x != 0 || veocity.y != 0)
         {
             transform.position += new Vector3(veocity.x * Time.deltaTime, veocity.y * Time.deltaTime, 0.0f);
